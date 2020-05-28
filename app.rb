1501f5570770242bc6bea68deadcdecf424389c6
Oleg-rb <oleg1107@rambler.ru>
2020-05-28 19:24:54 +0300

    Update logger

--- app.rb
+++ app.rb
@@ -1,7 +1,18 @@
 class Logger
-  def self.say_something
+  def initialize
+    @f = File.open 'log.txt', 'a'
+  end
+
+  def self.say_something         #class method
     puts "haha"
+  end
+
+  def log_something wat          #instance method
+    @f.puts wat
   end
 end
 
-Logger.say_something
+Logger.say_something            #вызов для class method
+
+logger = Logger.new             #вызов для instance method
+logger.log_something 'hey!'
