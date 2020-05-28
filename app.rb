131273f6bb8a5ffb9aac1020244f37489ac82022
Oleg-rb <oleg1107@rambler.ru>
2020-05-28 21:04:23 +0300

    Update logger to include self.instance

--- app.rb
+++ app.rb
@@ -3,16 +3,28 @@ class Logger
     @f = File.open 'log.txt', 'a'
   end
 
-  def self.say_something         #class method
+@@x = nil
+
+  def self.instance             #метод Singleton обычно использует в названии слово "instance"
+    if @@x == nil
+      @@x = Logger.new
+    end
+
+    return @@x
+  end
+
+  def self.say_something        #class method
     puts "haha"
   end
 
-  def log_something wat          #instance method
+  def log_something wat         #instance method
     @f.puts wat
   end
 end
 
 Logger.say_something            #вызов для class method
+Logger.instance.log_something 'blabla'
+Logger.instance.log_something 'blabla2'
 
 logger = Logger.new             #вызов для instance method
 logger.log_something 'hey!'
