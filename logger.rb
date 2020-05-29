33f334192ace6479086a1664f3ce7926dd64e7cb
Oleg-rb <oleg1107@rambler.ru>
2020-05-29 16:34:12 +0300

    Used stsndart Singleton

--- logger.rb
+++ logger.rb
@@ -1,17 +1,14 @@
+require 'singleton'
+
 class Logger
+  include Singleton
+
   def initialize
     @f = File.open 'log.txt', 'a'
   end
 
-  @@x = Logger.new
-
-  def self.instance             #метод Singleton обычно использует в названии слово "instance"
-    return @@x
-  end
-
   def log_something wat         #instance method
     @f.puts wat
+    @f.flush                    #все изминения будут сброшены на диск
   end
-
-  private_class_method :new     # метод new может вызываться только внутри класса, а не из вне
 end
